c49a613dfe57648c1edbff8ca521cb766cdb8508
caetanoct <42066749+caetanoct@users.noreply.github.com>
2018-10-29 21:57:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,8 @@
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
-// Import the discord.js module
-const Discord = require('discord.js');
+// Extract the required classes from the discord.js module
+const { Client, Attachment } = require('discord.js');
 
 // Create an instance of a Discord client
-const client = new Discord.Client();
+const client = new Client();
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -16,13 +12,14 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
-  }
+    // If the message is '!rip'
+    if (message.content === '!rip') {
+        // Create the attachment using Attachment
+        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        // Send the attachment in the message channel
+        message.channel.send(attachment);
+    }
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
