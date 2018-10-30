c1ec9f056104f4fd1985da754fe8c2e25941fd9b
caetanoct <42066749+caetanoct@users.noreply.github.com>
2018-10-29 22:29:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,4 @@
-
 const Discord = require('discord.js');
-
-
-const client = new Discord.Client();
-const prefix = '.';
-var frasesDoGuedes = ["eu demorei 18 anos pra pegar meu primeiro onibus",
-"escuto sertanejo toda hora pink floyd banda de viado, preto nao presta,  indio nao presta, o que presta é o pastor empresario trabalhadors",
-"com medo da nova era petista?",
-"eu voto no bolsonaro porque apoio o discurso de odio contra as minorias",
-"chora esquerdalha",
-"olha pra mim faço eng civil na udesc"];
-
-function getRandomFrase(){
-	var randomNumber = Math.floor(Math.random()*frasesDoGuedes.length);const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.';
 var frasesDoGuedes = ["eu demorei 18 anos pra pegar meu primeiro onibus",
@@ -38,7 +24,7 @@ client.on('message', message => {
     if (message.content === prefix+'top') {
     	message.channel.send('topper');
   	}
-    if(message.content == prefix+'ping'){ 
+    if(message.content == prefix+'ping'){ // Check if message is "!ping"
 	message.channel.send("Pinging ...") // Placeholder for pinging ... 
 	.then((msg) => { // Resolve promise
 		msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
@@ -48,31 +34,5 @@ client.on('message', message => {
 	message.reply(message.author.avatarURL);
     	}
 });
-
-client.login(process.env.BOT_TOKEN);
-
-	return frasesDoGuedes[randomNumber];	
-}
-
-client.on('ready', () => {
-  console.log('I am ready2');
-});
-
-client.on('message', message => {
-  if (message.content === prefix+'ping') {
-    message.channel.send('pong');
-  }
-	if (message.content === prefix+'guedes') {
-    	message.channel.send(getRandomFrase());
-  	}
-    if (message.content === prefix+'top') {
-    	message.channel.send('topper');
-  	}
-   
-    if (message.content === prefix+'avatar') {
-	message.reply(message.author.avatarURL);
-    	}
-});
-
 
 client.login(process.env.BOT_TOKEN);
